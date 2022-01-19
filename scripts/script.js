550de3f9123dcb51ccd5bf8a53584d3714942ce9
Hebaghozlan <94401320+Hebaghozlan@users.noreply.github.com>
2022-01-19 23:19:57 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,4 @@
+//HAMBURGER MENU
 var deButton = document.querySelector("nav button");
 
 deButton.addEventListener("click", toggleMenu);
@@ -7,6 +8,7 @@ function toggleMenu(event) {
   deNav.classList.toggle("toonMenu");
 }
 
+//SCROLL ANIMATIE
 //CREDIT VAN DEZE CODE NAAR SAM SLOTEMAKER
 
 //ELEMENTEN DIE JE WILT LATEN FADEN!
@@ -35,3 +37,42 @@ const observer = new IntersectionObserver(callbackFunction, options)
 allSections.forEach(item => {
     observer.observe(item)
 })
+
+// RESPONSIVE DISSCLOSUE
+// ROEP ALLE ELEMENTEN AAN
+const radio1 = document.getElementById("radio1")
+const radio2 = document.getElementById("radio2")
+const radio3 = document.getElementById("radio3")
+const input = document.querySelector(".input")
+
+
+// ZIET WANNEER RADIO3 IS GESELECTEERD
+radio3.addEventListener("click", function(){
+
+    // VOEGT DE CLASS "VISIBLE" TOE AAN HET INPUT FIELD
+    input.classList.add("visible")
+})
+
+// zZIET WANNEER RADIO1 EN RADIO 2 ZIJN GESELECTEERD
+radio2.addEventListener("click", function(){
+    input.classList.remove("visible")
+})
+radio1.addEventListener("click", function(){
+    input.classList.remove("visible")
+})
+
+// REFRESH PAGINA, KRIJG JE EEN ANDER PLAATJE
+var images = [
+    "images/code.png",
+    "images/code1.png",
+    "images/code2.png",
+    "images/code3.png"]
+
+
+function randImg() {
+    var size = images.length
+    var x = Math.floor(size * Math.random())
+    document.getElementById('image').src = images[x];
+}
+
+randImg();
